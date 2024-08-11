c327eafefd31c35295ec6669b26fd68c9f08b2a8
esafronov <egor-www@yandex.ru>
2024-08-11 15:55:28 +0300

    set default server address :8080

--- internal/agent/flags.go
+++ internal/agent/flags.go
@@ -9,7 +9,7 @@ var flagPollInterval int
 var flagReportInterval int
 
 func parseFlags() {
-	flag.StringVar(&flagServerAddress, "a", "localhost:8080", "address and port to send reports")
+	flag.StringVar(&flagServerAddress, "a", ":8080", "address and port to send reports")
 	flag.IntVar(&flagPollInterval, "p", 2, "poll interval in seconds")
 	flag.IntVar(&flagReportInterval, "r", 10, "report interval in seconds")
 	flag.Parse()
